bde2e120fcd3404aa5c5fd0bcadb9144859152e0
Matt McCartney <mlm071604@hotmail.com>
2022-08-03 06:56:06 -0400

    Added data collection

--- src/main.py
+++ src/main.py
@@ -2,11 +2,25 @@
 
 """Find the currently active window."""
 
+from datetime import datetime
 import logging
+import os
 import re
 import sys
 
-from time import sleep
+import time
+import pandas as pd
+import sqlite3
+
+
+db = sqlite3.connect("data.db")
+cur = db.cursor()
+
+try:
+    cur.execute('''CREATE TABLE screentime
+                (app text, start text, end text, seconds integer)''')
+except:
+    pass
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.DEBUG,
@@ -54,7 +68,6 @@ def get_active_window():
                     active_window_name = f.read()
     elif sys.platform in ['Windows', 'win32', 'cygwin']:
         import win32gui, win32process, win32api, win32con
-        import psutil
         window = win32gui.GetForegroundWindow()
         pid = win32process.GetWindowThreadProcessId(window)
         try:
@@ -88,11 +101,49 @@ def get_active_window():
         active_window_name = window_bundle_url.split(".app")[0].split("/")[-1].replace("%20", " ")
         date = NSDate.date()
         rl.acceptInputForMode_beforeDate_(NSDefaultRunLoopMode, date)
+        if active_window_name in ["Finder", "loginwindow"]:
+            active_window_name = None
     else:
         print(f"{sys.platform=} is unknown. Please report.")
         print(sys.version)
     return active_window_name
 
+data = []
+active_window = get_active_window()
+start_time = time.time()
 while True:
-    print("Active window: %s" % str(get_active_window()))
-    sleep(1)
+    data_size = len(data)
+    try:
+        time.sleep(1)
+        new_window = get_active_window()
+        if active_window != new_window:
+            if active_window != None:
+                end_time = time.time()
+                seconds_elapsed = end_time - start_time
+                start_date = datetime.fromtimestamp(start_time)
+                end_date = datetime.fromtimestamp(end_time)
+                cur.execute("INSERT INTO screentime values (?, ?, ?, ?)", (active_window, start_date, end_date, int(seconds_elapsed)))
+                data.append([active_window, start_date, end_date, seconds_elapsed])
+            active_window = new_window
+            start_time = time.time()
+    except KeyboardInterrupt:
+        if active_window != None:
+            end_time = time.time()
+            seconds_elapsed = end_time - start_time
+            start_date = datetime.fromtimestamp(start_time)
+            end_date = datetime.fromtimestamp(end_time)
+            cur.execute("INSERT INTO screentime values (?, ?, ?, ?)", (active_window, start_date, end_date, int(seconds_elapsed)))
+            data.append([active_window, start_date, end_date, seconds_elapsed])
+        break
+
+    if len(data) != data_size:
+        session_df = pd.DataFrame(data, columns=["App", "Start", "End", "Total Seconds"])
+        purge = "cls" if sys.platform in ['Windows', 'win32', 'cygwin'] else "clear"
+        os.system(purge)
+        print(session_df.to_string())
+
+    db.commit()
+
+session_df = pd.DataFrame(data, columns=["App", "Start", "End", "Total Seconds"])
+session_df.to_csv("data.csv")
+session_df.to_json("data.json")
